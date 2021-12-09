2cd66dcc1730e6fcc5e53e23df172bf0a1b52392
Linda Martinez <lindanmjw@gmail.com>
2021-12-08 20:27:36 -0500

    adding Quote API to js

--- server.js
+++ server.js
@@ -105,3 +105,32 @@ db.sequelize.sync().then(function() {
 // app.get('/', (req, res) =>{ 
 //   res.render('day.handlebars', {name: christian})
 // })
+
+
+// Quote API Function
+function getQuote() {
+  fetch("https://type.fit/api/quotes")
+      .then(res => {
+          return res.json()
+      })
+      .then(data => {
+          // Choose a random quote from quote array (data comes back as an array of objects which contain text and author)
+          let chosenQuote = data[Math.floor(Math.random() * data.length)]
+          let author = ''
+          if (chosenQuote.author == null) {
+              author = 'anonymous'
+          } else {
+              author = chosenQuote.author
+          }
+
+          quoteContainer.addClass('animate__fadeIn')
+          setTimeout(() => {
+              quoteContainer.removeClass('animate__fadeIn')
+          }, 1000)
+
+          quoteContainer.empty()
+          quoteContainer.append(`"${chosenQuote.text}" -${author}`)
+      })
+}
+
+getQuote()
