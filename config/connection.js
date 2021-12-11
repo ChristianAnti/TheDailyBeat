9668f2ef6b970300714dff20bc39fde7057e4cfd
Linda Martinez <lindanmjw@gmail.com>
2021-12-11 11:28:21 -0500

    added user.js model and cleaned up folders that had unnecessary files

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.user_db,
-  process.env.root,
-  process.env.root,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
-    port: 8080,
+    port: 3306,
   }
 );
 
